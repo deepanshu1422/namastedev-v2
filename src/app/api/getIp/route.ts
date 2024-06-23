b3e031626aa550ff12a8e3bdb4c759dc15847916
Rakshit Raj <404rakshit@gmail.com>
2024-06-24 02:42:44 +0530

    test: edge

--- src/app/api/getIp/route.ts
+++ src/app/api/getIp/route.ts
@@ -1,8 +1,27 @@
-import { geolocation } from '@vercel/edge';
- 
-export function GET(request: Request) {
-  const { city } = geolocation(request);
-  return new Response(`<h1>Your location is ${city}</h1>`, {
-    headers: { 'content-type': 'text/html' },
-  });
+// api/index.js
+// Import the geolocation and ipAddress helpers
+import { geolocation, ipAddress } from "@vercel/edge";
+import { NextRequest } from "next/server";
+
+export const config = {
+	runtime: "edge",
+};
+
+export default function (request: NextRequest) {
+	// The geolocation helper pulls out the geoIP headers from the
+	const geo = geolocation(request) || {};
+	// The IP helper does the same function for the user's IP address
+	const ip = ipAddress(request) || null
+
+	// Output the Geolocation data and IP address as a JSON object, and
+	// set the content type to make it easier to handle when requested
+	return new Response(
+		JSON.stringify({
+			...geo,
+			ip,
+		}),
+		{
+			headers: { "content-type": "application/json" },
+		}
+	);
 }
