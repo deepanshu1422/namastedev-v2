e8bb568f84d6fb61c506297ef6e269c5e47f864e
Rakshit Raj <404rakshit@gmail.com>
2024-06-25 23:54:25 +0530

    Update render.tsx

--- src/app/(main)/mentorship/render.tsx
+++ src/app/(main)/mentorship/render.tsx
@@ -5,11 +5,11 @@ import Link from 'next/link'
 export default function Render() {
     const countryCode = headers().get('x-vercel-ip-country') || 'US'
 
-    if (countryCode === "IN") return <div className='flex flex-col gap-2'>
+    if (countryCode === "NL") return <div className='flex flex-col gap-2'>
         <span>{countryCode}</span>
         <Link href={"https://pages.razorpay.com/pl_NRwJhRPeyZEekG/view"} target='_blank' className='bg-prime rounded-full px-4 text-center py-3 max-w-3xl mx-auto w-full text-3xl sm:text-4xl font-extrabold flex gap-3 justify-center hover:opacity-80 transition-all duration-200 uppercase'><CreditCard className="h-10 w-10" />Join Now</Link></div>
 
-    if (countryCode !== "IN") return (
+    if (countryCode !== "NL") return (
         <div className='flex flex-col gap-2'>
             <span>{countryCode}</span>
             <Link href={"https://hotshotpanda.myshopify.com/cart/46100616151210:1?channel=buy_button"} target='_blank' className='bg-prime rounded-full px-4 text-center py-3 max-w-3xl mx-auto w-full text-3xl sm:text-4xl font-extrabold flex gap-3 justify-center hover:opacity-80 transition-all duration-200 uppercase'><CreditCard className="h-10 w-10" />Join Now</Link>
