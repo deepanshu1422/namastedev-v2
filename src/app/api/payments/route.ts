f8abc2f8d7af1d02f230003ab76a861007602f75
Rakshit Raj <404rakshit@gmail.com>
2024-09-03 01:36:06 +0530

    test: UTC2

--- src/app/api/payments/route.ts
+++ src/app/api/payments/route.ts
@@ -1,9 +1,19 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    
-    const serverDate = new Date(Date.now())
-    const localString = serverDate.toLocaleString()
-    
-    return NextResponse.json({ serverDate, localString });
+  const serverDate = new Date(Date.now());
+  const localString = serverDate.toLocaleString();
+
+  // Convert UTC to IST
+  const offset = 5.5 * 60 * 60 * 1000; // 5.5 hours in milliseconds
+  const serverDateIST = new Date(serverDate.getTime() + offset);
+
+  // Format to local string (IST)
+  const localStringIST = serverDateIST.toLocaleString("en-IN", {
+    timeZone: "Asia/Kolkata",
+  });
+
+  console.log("Server Date in IST: ", localStringIST);
+
+  return NextResponse.json({ serverDate, localString, serverDateIST, localStringIST });
 }
