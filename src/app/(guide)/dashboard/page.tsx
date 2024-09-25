c6deeba8755ec4ae8b23c6318c4ac439b300f41f
Rakshit Raj <404rakshit@gmail.com>
2024-09-26 01:30:08 +0530

    fix: dashboard

--- src/app/(guide)/dashboard/page.tsx
+++ src/app/(guide)/dashboard/page.tsx
@@ -7,13 +7,9 @@ import { UserDialog } from "./new-user";
 import { auth } from "@/auth";
 import PurchaseTabs from "./purchased";
 
-import { Notebook, Terminal } from "lucide-react"
+import { Notebook, Terminal } from "lucide-react";
 
-import {
-  Alert,
-  AlertDescription,
-  AlertTitle,
-} from "@/components/ui/alert"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import Link from "next/link";
 
 export const dynamic = "force-dynamic";
@@ -41,9 +37,7 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default async function Dashboard() {
-
   return (
     <div className="flex h-full relative overflow-hidden">
       <div className="flex-1 flex flex-col gap-4 px-6 py-5 lg:px-8 w-full max-w-6xl mx-auto overflow-hidden">
@@ -52,6 +46,7 @@ export default async function Dashboard() {
           <PurchaseTabs />
         </div>
         <Suspense fallback={<CoursesFallback />}>
+          <span className="md:text-lg font-bold">Other Courses</span>
           <Courses />
         </Suspense>
       </div>
@@ -77,13 +72,14 @@ function CoursesFallback() {
 export function OldCourses() {
   return (
     <Link target="_blank" href={"https://courses.30dayscoding.com/s/store"}>
-    <Alert className="border-prime/80 bg-gradient-to-b from-prime/60 to-second/60 shadow-lg">
-      <Notebook className="h-4 w-4" />
-      <AlertTitle>Note</AlertTitle>
-      <AlertDescription>
-        You can access all your previous bought courses <span className="text-primary underline">here.</span>
-      </AlertDescription>
-    </Alert>
-        </Link>
-  )
+      <Alert className="border-prime/80 bg-gradient-to-b from-prime/60 to-second/60 shadow-lg">
+        <Notebook className="h-4 w-4" />
+        <AlertTitle>Note</AlertTitle>
+        <AlertDescription>
+          You can access all your previous bought courses{" "}
+          <span className="text-primary underline">here.</span>
+        </AlertDescription>
+      </Alert>
+    </Link>
+  );
 }
