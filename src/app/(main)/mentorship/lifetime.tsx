529f65ea7722585ae7895acdff88f7241dad301a
Aryan Singh <aspiringaryan@gmail.com>
2024-10-14 13:59:29 -0700

    update ui - main

--- src/app/(main)/mentorship/lifetime.tsx
+++ src/app/(main)/mentorship/lifetime.tsx
@@ -9,7 +9,6 @@ import { ChevronDownCircle } from "lucide-react";
 
 export const benefits = [
   "25+ courses, 150+ hours of content, 40+ guides",
-  "Live classes on DSA, Full Stack Development, and more",
   "Regular Q&A calls with Aryan, Ashok, and Deepanshu",
   "24/7 WhatsApp access for any doubts and queries",
   "Access to a Thriving Community + Networking Opportunities",
@@ -25,21 +24,21 @@ export default function Lifetime({
       <span className="flex items-center justify-center gap-4 relative pb-4">
         <hr className="max-phone:hidden h-0.5 max-lg:w-20 w-60 max-w-60 rounded bg-gradient-to-r from-0% from-transparent to-100% to-prime" />
         <h1 className="font-jakarta bg-gradient-to-r from-lime-300 to-green-800 text-transparent bg-clip-text leading-tight text-[2.3rem] sm:text-6xl font-extrabold text-center">
-        Join tech job mentorship community (lifetime)
+        Join USA Canada jobs community
         </h1>
         <hr className="max-phone:hidden h-0.5 max-lg:w-20 w-60 max-w-60 rounded bg-gradient-to-l from-0% from-transparent to-100% to-prime" />
       </span>
       
-      <section className="flex mx-auto flex-wrap text-2xl w-full max-md:text-sm items-center justify-center md:divide-x-2 divide-white py-2">
+      {/* <section className="flex mx-auto flex-wrap text-2xl w-full max-md:text-sm items-center justify-center md:divide-x-2 divide-white py-2">
         <span className="px-1 md:px-4">Lifetime access to community</span>
         <span className="md:hidden text-prime font-bold">&</span>
         <span className="px-1 md:px-4">250+ joined this month</span>
         <span className="md:hidden text-prime font-bold">&</span>
         <span className="px-1 md:px-4">20+ Job offers last month</span>
 
-      </section>
+      </section> */}
 
-      <Btn cover="/new-mentorship.jpg" yt="9Lokc1bQixc" />
+      {/* <Btn cover="/new-mentorship.jpg" yt="9Lokc1bQixc" /> */}
       <div className="flex flex-col items-center py-4 mx-auto text-xl">
         {benefits?.map((e, i) => {
           const words = e.split(" ");
@@ -72,10 +71,10 @@ export default function Lifetime({
       </p> */}
       
       <div className="px-6 lg:px-10">
-        <Link href={"#apply"} className="mx-auto">
+        <Link href={"https://www.skool.com/usa-ca-jobs/about"} target="_blank" className="mx-auto">
           <button className="mt-10 border-4 border-prime rounded-full px-4 text-center py-2 max-w-3xl mx-auto w-full text-2xl sm:text-2xl font-extrabold flex gap-3 justify-center hover:opacity-80 transition-all duration-200 uppercase">
-            <ChevronDownCircle className="h-8 w-8" />
-            Fill the form
+            {/* <ChevronDownCircle className="h-8 w-8" /> */}
+            Join community
           </button>
         </Link>
       </div>
