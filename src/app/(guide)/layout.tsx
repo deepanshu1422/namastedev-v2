7229742250f9df3751c3c0da170b5e81091e2e11
Rakshit Raj <404rakshit@gmail.com>
2024-10-16 15:48:46 +0530

    fix: static-auth

--- src/app/(guide)/layout.tsx
+++ src/app/(guide)/layout.tsx
@@ -1,13 +1,17 @@
+import { SessionProvider } from "next-auth/react";
 import Menu from "./menu";
+import { auth } from "@/auth";
 
-export default function GuideLayout({
+export default async function GuideLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
   return (
-    <div>
-      {children}
-    </div>
+    <SessionProvider session={session}>
+      <div>{children}</div>
+    </SessionProvider>
   );
 }
