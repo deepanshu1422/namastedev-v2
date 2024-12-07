aed55d12ba4bb072c756ff66a09da1e5aeb0a3e9
Rakshit Raj <404rakshit@gmail.com>
2024-12-08 01:47:16 +0530

    add: popup

--- src/app/(main)/blog/[slug]/page.tsx
+++ src/app/(main)/blog/[slug]/page.tsx
@@ -10,7 +10,10 @@ import { BASE_URL } from "@/util/constants";
 import CopyBtn from "./copyBtn";
 import { compileMDX, MDXRemote } from "next-mdx-remote/rsc";
 import YoutubeEmbed from "@/components/yotube-embed";
-import { FooterAD, SquareAD } from "@/util/ads";
+// import { FooterAD, SquareAD } from "@/util/ads";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Notebook } from "lucide-react";
+import Banner from "./banner"
 
 export const dynamicParams = true;
 export const dynamic = "force-static";
@@ -154,7 +157,7 @@ export default async function Home({ params: { slug } }: PageProps) {
     });
 
     return (
-      <main className="bg-background bg-bg min-h-svh transition-all">
+      <main className="bg-background bg-bg min-h-svh transition-all flex flex-col">
         <Hero
           title={item.title ?? ""}
           desc={item.description ?? ""}
@@ -170,16 +173,30 @@ export default async function Home({ params: { slug } }: PageProps) {
           createdAt={item.createdAt}
           tag={item.tags[0] ?? item.focusKeyword[0]}
         />
-  
-        <section className="flex max-md:flex-col gap-2">
-          <SquareAD />
+
+        <section className="flex max-md:flex-col">
+          {/* <SquareAD /> */}
           <div className="max-w-lg md:max-w-3xl m-auto px-8 lg:px-5 body">
             {content}
           </div>
-          <SquareAD />
+          {/* <SquareAD /> */}
         </section>
-  
-        <div className="py-10 m-auto px-8 lg:px-5 grid gap-4 max-w-lg md:max-w-3xl">
+
+        <Link
+          className="mx-auto p-5"
+          target="_blank"
+          href={`https://30dayscoding.com/bundle/complete-package-all-course-bundle?utm_source=${"https://30dayscoding.com/blog/" + slug}&utm_medium=blog&utm_campaign=banner`}
+        >
+          <Alert className="border-prime/80 bg-gradient-to-b from-prime/60 to-second/60 shadow-lg text-left max-w-2xl md:max-w-3xl gap-1.5 flex flex-col">
+            <AlertTitle className="font-bold md:text-lg">🎉 Exclusive Deal Alert!  17 premium courses for just ₹999.</AlertTitle>
+            <AlertDescription className="max-md:text-xs">
+              Unlock your potential with our expert-led courses—master skills,
+              earn certificates, and transform your career today! <span className="text-primary underline">here.</span>
+            </AlertDescription>
+          </Alert>
+        </Link>
+
+        <div className="pb-10 m-auto px-8 lg:px-5 grid gap-4 max-w-lg md:max-w-3xl">
           <span className="text-2xl max-md:text-xl font-bold break-words">
             Share Article
           </span>
@@ -197,7 +214,7 @@ export default async function Home({ params: { slug } }: PageProps) {
                 <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
               </svg>
             </Link>
-  
+
             <Link
               href={`mailto:?subject=Do check our latest blog on '${item.title}'&body=Check out new blog on ${item.title} via visiting - https://30dayscoding/blog/${slug}.`}
               title={"Share via Mail"}
@@ -210,9 +227,9 @@ export default async function Home({ params: { slug } }: PageProps) {
                 <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
               </svg>
             </Link>
-  
+
             <CopyBtn slug={slug} />
-  
+
             <Link
               href={`https://twitter.com/intent/tweet?text=Read the blog from 30dayscoding on ${item.title} - ${BASE_URL}/blog/${slug}`}
               title={"Share via X"}
@@ -228,7 +245,7 @@ export default async function Home({ params: { slug } }: PageProps) {
             </Link>
           </section>
           <hr className="border-gray-500 my-3" />
-          <FooterAD />
+          {/* <FooterAD /> */}
           <span className="text-2xl max-md:text-xl font-bold break-words">
             Latest Blogs
           </span>
@@ -240,7 +257,7 @@ export default async function Home({ params: { slug } }: PageProps) {
                 month: "long",
                 day: "numeric",
               });
-  
+
               return (
                 <Link
                   key={i}
@@ -269,11 +286,10 @@ export default async function Home({ params: { slug } }: PageProps) {
             })}
           </div>
         </div>
+        <Banner slug={slug} />
       </main>
     );
   } catch (error) {
-    return notFound()
+    return notFound();
   }
-
-  
 }
