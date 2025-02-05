e30bd8c7b1f535220eba9a900ac6af3f2b7df7ca
singh1aryan <aspiringaryan@gmail.com>
2025-02-04 23:22:19 -0800

    content - courses

--- src/app/(main)/courses/main.tsx
+++ src/app/(main)/courses/main.tsx
@@ -117,13 +117,13 @@ export default function Main({ courses }: { courses: CoursesType }) {
         }`,
         }}
       />
-      <Hero
+      {/* <Hero
         title="Job-Ready Mastery Courses"
         desc="Advance your development skills with our comprehensive online courses. Learn Full Stack, Data Structures & Algorithms, AI, Blockchain, Data Analytics, and more from industry experts."
         heroImage="https://images.unsplash.com/photo-1517411032315-54ef2cb783bb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         search={state}
         setSearch={setState}
-      />
+      /> */}
       <Bundle
         content={bundle?.offers ?? [""]}
         title={bundle?.bundleTitle ?? ""}
