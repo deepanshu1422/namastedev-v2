add6b05bbb4bd45eabb3fcb2e64f416780e8f3e7
deepanshu <deepanshu1422@gmail.com>
2025-03-08 23:43:36 +0530

    Added - Google Tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,15 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-P3K58FHB');`}
+        </Script>
+        {/* End Google Tag Manager */}
         <GoogleAnalytics gaId="G-4M8L4RM0LB" />
         <MixpanelAnalytics
           projectToken={process.env.NEXT_PUBLIC_MIXPANEL_TOKEN!}
@@ -47,6 +56,16 @@ export default async function RootLayout({
               <body
                 className={`${outfit.className} ${bric.variable} bg-bg`}
               >
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                  <iframe 
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-P3K58FHB"
+                    height="0" 
+                    width="0" 
+                    style={{ display: 'none', visibility: 'hidden' }}
+                  ></iframe>
+                </noscript>
+                {/* End Google Tag Manager (noscript) */}
                 <StreakTracker />
                 {children}
                 <Toaster richColors />
