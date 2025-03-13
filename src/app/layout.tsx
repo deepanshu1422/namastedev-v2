72877b7e69dc1336351b7e2f4c5d4e4b500445c3
deepanshu <deepanshu1422@gmail.com>
2025-03-13 11:58:17 +0530

    Pixel firing once , page view working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,29 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        
+        {/* Facebook Pixel Code */}
+        {/* TODO: Replace '{your-pixel-id-goes-here}' with your actual Meta Pixel ID from Facebook Ads Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1400030234779972');
+            fbq('track', 'PageView');
+          `
+        }} />
+        <noscript dangerouslySetInnerHTML={{
+          __html: `
+            <img height="1" width="1" style="display:none" 
+            src="https://www.facebook.com/tr?id=1400030234779972&ev=PageView&noscript=1"/>
+          `
+        }} />
+        {/* End Facebook Pixel Code */}
        
       </head>
     
